3a5c822385791adf79ef1373697d419faecd5925
Rooam Lee <leewr9@naver.com>
2025-04-29 16:49:47 +0900

    plugins:: fix upload rule

--- plugins/database.py
+++ plugins/database.py
@@ -7,11 +7,13 @@ def upload_to_database(pathname, table_name, schema_name="public"):
     Load data from the specified bucket and directory into a PostgreSQL database.
     """
     engine = create_engine("postgresql://postgres:postgres@host.docker.internal:5432/postgres") 
+    with engine.connect() as conn:
+        conn.execute(f"DROP TABLE {schema_name}.{table_name} CASCADE;")
 
     df_list = []
     for file_path in glob.glob(pathname):
         print(f"File Path: {file_path}")
         df_list.append(pd.read_parquet(file_path))
-
+    
     df = pd.concat(df_list, ignore_index=True)
-    df.to_sql(table_name, engine, schema=schema_name, if_exists="replace", index=False)
+    df.to_sql(table_name, engine, schema=schema_name, if_exists="append", index=False)
