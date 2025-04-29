194650ba4058ee77aa52f2ee631b52975062d4fd
Rooam Lee <leewr9@naver.com>
2025-04-29 16:26:29 +0900

    dags:: add analytics task

--- dags/fetch_daily_data.py
+++ dags/fetch_daily_data.py
@@ -6,6 +6,7 @@ sys.path.insert(0, os.path.abspath("/opt/airflow"))
 sys.path.insert(0, os.path.abspath("/opt/airflow/collector"))
 
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 from collector.scrapers import game, player, schedule, team
@@ -14,22 +15,23 @@ from plugins.database import upload_to_database
 PROJECT_NAME = "kbo-data-project"
 BUCKET_NAME = "kbo-data"
 BUCKET_DIR = "players/weekly"
+ANALYTICS_DIR = '/opt/airflow/analytics'
 
 
 def load_data():
     """
     Load data from the specified bucket and directory.
     """
-    upload_to_database("collector/output/game/*/*/summary.parquet", "game_summary", "public")
-    upload_to_database("collector/output/game/*/*/hitter.parquet", "game_hitter_summary", "public")
-    upload_to_database("collector/output/game/*/*/pitcher.parquet", "game_pitcher_summary", "public")
+    upload_to_database("collector/output/game/*/*/summary.parquet", "game_summary")
+    upload_to_database("collector/output/game/*/*/hitter.parquet", "game_hitter_summary")
+    upload_to_database("collector/output/game/*/*/pitcher.parquet", "game_pitcher_summary")
 
-    upload_to_database("collector/output/player/*/hitter.parquet", "player_hitter_stats", "public")
-    upload_to_database("collector/output/player/*/pitcher.parquet", "player_pitcher_stats", "public")
-    upload_to_database("collector/output/player/*/runner.parquet", "player_runner_stats", "public")
-    upload_to_database("collector/output/player/*/fielder.parquet", "player_fielder_stats", "public")
+    upload_to_database("collector/output/player/*/hitter.parquet", "player_hitter_stats")
+    upload_to_database("collector/output/player/*/pitcher.parquet", "player_pitcher_stats")
+    upload_to_database("collector/output/player/*/runner.parquet", "player_runner_stats")
+    upload_to_database("collector/output/player/*/fielder.parquet", "player_fielder_stats")
 
-    upload_to_database("collector/output/schedule/*.parquet", "game_schedule", "public")
+    upload_to_database("collector/output/schedule/*.parquet", "game_schedule")
 
 
 def run_season_scraper(**kwargs):
@@ -80,4 +82,10 @@ with DAG(
         dag=dag
     )
 
-    [run_season_scraper_task, run_date_scraper_task] >> load_data_task
+    analytics_data_task = BashOperator(
+        task_id="analytics_data",
+        bash_command=f"cd {ANALYTICS_DIR} &&" \
+                     f"dbt run --project-dir {ANALYTICS_DIR} --profiles-dir {ANALYTICS_DIR}"
+    )
+
+    [run_season_scraper_task, run_date_scraper_task] >> load_data_task >> analytics_data_task
