e4926113db961e015a949cc5c49529c262386c47
Rooam Lee <leewr9@naver.com>
2025-05-09 14:51:32 +0900

    plugins:: fix store func

--- plugins/database.py
+++ plugins/database.py
@@ -1,19 +1,50 @@
 import glob
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-def upload_to_database(pathname, table_name, schema_name="public"):
-    """
-    Load data from the specified bucket and directory into a PostgreSQL database.
-    """
-    engine = create_engine("postgresql://postgres:postgres@host.docker.internal:5432/postgres") 
+
+def _get_engine():
+    """Create and return the SQLAlchemy engine."""
+    return create_engine("postgresql://postgres:postgres@host.docker.internal:5432/postgres")
+
+def _insert_to_db(pathname: str, table: str, schema: str = "public"):
+    """Insert data from files into the specified database table."""
+    engine = _get_engine()
     with engine.connect() as conn:
-        conn.execute(f"DROP TABLE {schema_name}.{table_name} CASCADE;")
+        conn.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
+        conn.execute(f"DROP TABLE IF EXISTS {schema}.{table} CASCADE;")
 
-    df_list = []
-    for file_path in glob.glob(pathname):
-        print(f"File Path: {file_path}")
-        df_list.append(pd.read_parquet(file_path))
-    
-    df = pd.concat(df_list, ignore_index=True)
-    df.to_sql(table_name, engine, schema=schema_name, if_exists="append", index=False)
+        df_list = [pd.read_parquet(file_path) for file_path in glob.glob(pathname)]
+        if not df_list:
+            print(f"No parquet files found for {pathname}. Skipping...")
+            return
+        
+        df = pd.concat(df_list, ignore_index=True)
+        df.to_sql(table, engine, schema=schema, if_exists="append", index=False)
+        print(f"Inserted data into {schema}.{table} from {len(df_list)} parquet files.")
+
+
+def insert_game_data_to_db():
+    """Insert various game and player data into the database."""
+    _insert_to_db("output/processed/game/schedule/*/*.parquet", "schedule", "game")
+    _insert_to_db("output/processed/game/result/*/*.parquet", "result", "game")
+
+    for pt in ["hitter", "pitcher", "fielder", "runner"]:
+        _insert_to_db(
+            pathname=f"output/processed/player/*/{pt}/season_summary.parquet", 
+            table=f"{pt}_season_summary", 
+            schema="player"
+        )
+        
+        _insert_to_db(
+            pathname=f"output/processed/player/*/{pt}/*/daily.parquet", 
+            table=f"{pt}_daily_stats", 
+            schema="player"
+        )
+        
+        _insert_to_db(
+            pathname=f"output/processed/player/*/{pt}/*/situation.parquet", 
+            table=f"{pt}_situation_stats", 
+            schema="player"
+        )
+        
--- plugins/storage.py
+++ plugins/storage.py
@@ -1,45 +1,9 @@
-import os
-import pandas as pd
-
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
 
-def upload_to_gcs_helper(bucket_name, file_path, gcs_path):
-    """
-    Helper function to upload data to Google Cloud Storage (GCS).
-    """
+def _upload_to_cloud_storage(bucket_name, file_path, gcs_path):
     hook = GCSHook(gcp_conn_id="google_cloud_default")
-    df = pd.read_parquet(file_path)
-
-    print("Column Types:")
-    print(df.dtypes)
-    
-    print("Preview Rows:")
-    print(df.head())
-
-    print(f"GCS Path:  {bucket_name}/{gcs_path}")
-    print(f"File Path: {file_path}")
-
     hook.upload(bucket_name=bucket_name, object_name=gcs_path, filename=file_path)
 
-def upload_to_historical_gcs(bucket_name, bucket_dir, output_dir, filename, **kwargs):
-    """
-    Uploads the historical data to Google Cloud Storage (GCS).
-    """
-    execution_date = kwargs['execution_date']
-    targer_season = str(execution_date.year)
 
-    file_path = os.path.join(output_dir, f"{targer_season}_{filename}")
-    gcs_path = os.path.join(bucket_dir, targer_season, filename)
-
-    upload_to_gcs_helper(bucket_name, file_path, gcs_path)
-
-def upload_to_gcs(bucket_name, bucket_dir, output_dir, filename, **kwargs):
-    """
-    Uploads the data to Google Cloud Storage (GCS).
-    """
-    execution_date = kwargs['execution_date']
-
-    file_path = os.path.join(output_dir, filename)
-    gcs_path = os.path.join(bucket_dir, execution_date.strftime("%Y/%m/%d"), filename)
-
-    upload_to_gcs_helper(bucket_name, file_path, gcs_path)
+def upload_data_to_cloud_storage():
+    pass
